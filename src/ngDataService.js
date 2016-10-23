b6d67940b6a6785fb46d3c0a721feb42ca566403
Andriyan <mifkata@dbox.homain.org>
2016-10-23 16:52:53 +0300

    Added minimal url validation for service methods that take url as an argument

--- src/ngDataService.js
+++ src/ngDataService.js
@@ -1,7 +1,32 @@
 (function() {
   'use strict';
 
+  var VALID_URL_REGEIX = /^((http(s)?\:\/\/)|\/)/;
+
+  var validateUrl = function(url) {
+    if(!VALID_URL_REGEIX.test(url)) {
+      var callee = arguments.callee.caller.toString();
+      throw 'ngDataService' + callee + '(url) - provided url is invalid';
+    }
+  }
+
   var ngDataService = function() {
+  };
+
+  ngDataService.prototype.get = function(url) {
+    validateUrl(url);
+  }
+
+  ngDataService.prototype.getLive = function(url) {
+    validateUrl(url);
+  }
+
+  ngDataService.prototype.remove = function(url) {
+    validateUrl(url);
+  };
+
+  ngDataService.prototype.removeAll = function() {
+
   };
 
   ngDataService.$inject = [];
--- tests/ngDataService.test.js
+++ tests/ngDataService.test.js
@@ -9,4 +9,37 @@ describe('ngDataService', function() {
   it('should exist', function() {
     expect(service).not.toBe(undefined);
   });
+
+  it('should have valid API methods', function() {
+    ['get', 'getLive', 'remove', 'removeAll'].map(function(method) {
+      expect(typeof service[method]).toBe('function');
+    });
+  });
+
+  // simple URL validation, otherwise string pattern validation is too complex
+  // and only clusters the demo
+  describe('get|getLive|remove(url) should accept only http/https or / prefixed strings', function() {
+    var invalidUrl = [
+      1, 0, -1, -1.11, 0.11, NaN, [], {}, null, undefined,
+      '', '   ', 'asd', 'httpz', 'httzps', 'a/'
+    ];
+
+    var validUrl = [
+      'http://', 'https://', '/'
+    ];
+
+    ['get', 'getLive', 'remove'].map(function(method) {
+      invalidUrl.map(function(url) {
+        it(method + '(url) with invalid url SHOULD throw', function() {
+          expect(service[method].bind(service, url)).toThrow();
+        });
+      });
+
+      validUrl.map(function(url) {
+        it(method + '(url) with valid url SHOULD NOT throw', function() {
+          expect(service[method].bind(service, url)).not.toThrow();
+        });
+      });
+    });
+  });
 });
