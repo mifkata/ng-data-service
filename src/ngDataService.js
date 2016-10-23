ab356226ea0c329cb9ae743a03dbdc13bfc05923
Andriyan <mifkata@dbox.homain.org>
2016-10-23 20:51:18 +0300

    Added get() read from cache, remove() and removeAll()

--- src/ngDataService.js
+++ src/ngDataService.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // @angular
-  ngDataService.$inject = ['$http'];
+  ngDataService.$inject = ['$http', '$window', '$q'];
 
   angular
     .module('ng-data-service', [])
@@ -11,6 +11,7 @@
   // @constants
   var VALID_URL_REGEIX = /^((http(s)?\:\/\/)|\/)/;
   var CACHE_PREFIX = 'lsc_';
+  var CACHE_REGEX = /^lsc_((http(s)?\:\/\/)|\/)/;
 
   // @private
   function validateUrl(url) {
@@ -20,20 +21,38 @@
     }
   }
 
-  function onSuccess(url, value) {
+  function onRequestSuccess(url, value) {
+    this.storage.setItem(CACHE_PREFIX + url, JSON.stringify(value));
+    return value;
   }
 
-  function onError(url, error, status) {
-    throw 'HTTP GET ' + url + ' failed with status code ' + status;
+  function onRequestError(url, error, status) {
+    console.error('HTTP GET [' + url + '] failed with status code ' + status);
+    return error;
   }
 
   // @constructor
-  function ngDataService($http) {
+  function ngDataService($http, $window, $q) {
     this.$http = $http;
+    this.storage = $window.localStorage;
+    this.$q = $q;
   };
 
   ngDataService.prototype.get = function(url) {
     validateUrl(url);
+
+    var cached = this.storage.getItem(CACHE_PREFIX + url);
+    if(cached) {
+      var deferred = this.$q.defer();
+      var resolution = {
+        data: JSON.parse(cached)
+      };
+
+      deferred.resolve(resolution);
+      return deferred.promise;
+    }
+
+    return this.getLive(url);
   }
 
   ngDataService.prototype.getLive = function(url) {
@@ -41,15 +60,20 @@
 
     return this.$http
       .get(url)
-      .success(onSuccess.bind(this, url))
-      .error(onError.bind(this, url));
+      .success(onRequestSuccess.bind(this, url))
+      .error(onRequestError.bind(this, url));
   }
 
   ngDataService.prototype.remove = function(url) {
     validateUrl(url);
+    this.storage.removeItem(CACHE_PREFIX + url);
   };
 
   ngDataService.prototype.removeAll = function() {
-
+    for(var item in this.storage) {
+      if(CACHE_REGEX.test(item)) {
+        this.storage.removeItem(item);
+      }
+    }
   };
 })();
--- tests/ngDataService.test.js
+++ tests/ngDataService.test.js
@@ -1,9 +1,42 @@
+'use strict';
 describe('ngDataService', function() {
-  var service;
+  var $http, $root, service, call, $windowMock;
+
+  var validUrl = '/some-test-url';
+  var responseMock = { temp1: 123, temp2: 'asd' };
+  var responseMockStringified = JSON.stringify(responseMock);
+
+  // copy const, since not exported
+  var CACHE_PREFIX = 'lsc_';
 
   beforeEach(module('ng-data-service'));
-  beforeEach(inject(function(ngDataService) {
+
+  beforeEach(function() {
+    $windowMock = {
+      localStorage: {
+        setItem: function() {},
+        getItem: function() {},
+        removeItem: function() {},
+
+        // mocked keys
+        'lsc_/': 'test',
+        'lsc_http://test': true,
+        'lsc_invalid': 'invalid',
+        invalid: false,
+        123: null
+      }
+    };
+  });
+
+  beforeEach(function() {
+    module(function($provide) {
+      $provide.value('$window', $windowMock);
+    });
+  });
+
+  beforeEach(inject(function(ngDataService, $rootScope) {
     service = ngDataService;
+    $root = $rootScope;
   }));
 
   it('should exist', function() {
@@ -44,17 +77,15 @@ describe('ngDataService', function() {
   });
 
   describe('getLive(validUrl)', function() {
-    var $http, call;
-    var validUrl = '/some-test-url';
-    var response = { test: 123 };
-
     beforeEach(inject(function($httpBackend) {
-      $http = $httpBackend
-      call = $http.when('GET', validUrl).respond(response);
+      $http = $httpBackend;
+      call = $http.when('GET', validUrl).respond(responseMock);
+
+      spyOn($windowMock.localStorage, 'setItem');
+      spyOn(console, 'error');
     }));
 
     afterEach(function() {
-      $http.flush();
       $http.verifyNoOutstandingExpectation();
       $http.verifyNoOutstandingRequest();
     });
@@ -62,21 +93,142 @@ describe('ngDataService', function() {
     it('should make an $http GET call', function() {
       $http.expectGET(validUrl);
       service.getLive(validUrl);
+      $http.flush();
     });
 
     it('should return a promise', function() {
       var req = service.getLive(validUrl);
       expect(req).not.toBe(undefined);
       expect(typeof req.then).toBe('function');
+      $http.flush();
     });
 
-    it('should be able to catch errors', function(done) {
+    it('should pass down errors and output them in the console', function() {
       call.respond(401, 'error');
+
       $http.expectGET(validUrl);
-      service.getLive(validUrl).catch(function(err) {
-        expect(err).toBe('error');
-        done();
+      var result = service.getLive(validUrl);
+
+      result.catch(function(error) {
+        expect(error.data).toBe('error');
+        expect(error.status).toBe(401);
+        expect(console.error).toHaveBeenCalled();
       });
+
+      $http.flush();
+    });
+
+    it('should pass down response values properly', function() {
+      $http.expectGET(validUrl);
+      var result = service.getLive(validUrl);
+
+      result.then(function(res) {
+        expect(res.data).toEqual(responseMock);
+      });
+
+      $http.flush();
+    });
+
+    describe('successful responses should be cached in localStorage', function() {
+      it('keys should have ' + CACHE_PREFIX + '<url> format', function() {
+        var result = service.getLive(validUrl);
+        result.then(function(res) {
+          var expectedPrefix = CACHE_PREFIX + validUrl;
+
+          expect(res.data).toEqual(responseMock);
+          expect($windowMock.localStorage.setItem)
+            .toHaveBeenCalledWith(expectedPrefix, responseMockStringified);
+        });
+
+        $http.flush();
+      });
+    });
+  });
+
+  describe('get(validUrl)', function() {
+    beforeEach(inject(function($httpBackend) {
+      $http = $httpBackend;
+      call = $http.when('GET', validUrl).respond(responseMock);
+    }));
+
+    beforeEach(function() {
+      spyOn(service, 'getLive');
+      spyOn($windowMock.localStorage, 'getItem')
+        .and.returnValue(responseMockStringified);
+    });
+
+    it('should always return a promise', function() {
+      var result = service.get(validUrl);
+      expect(result).not.toBe(undefined);
+      expect(typeof result.then).toBe('function');
+    });
+
+    it('promise from cache should contain proper parsed data value', function(done) {
+      service.get(validUrl)
+        .then(function(res) {
+          expect(res.data).toEqual(responseMock);
+          done();
+        });
+
+      $root.$digest();
+    });
+
+    it('should always check the storage for cache and return if found', function() {
+      service.get(validUrl);
+      service.get(validUrl);
+
+      expect(service.getLive).not.toHaveBeenCalled();
+      expect($windowMock.localStorage.getItem)
+        .toHaveBeenCalledWith(CACHE_PREFIX + validUrl);
+
+      expect($windowMock.localStorage.getItem.calls.count()).toBe(2);
+    });
+
+    it('should call getLive() if the cache is empty', function() {
+      $windowMock.localStorage.getItem.and.returnValue(undefined);
+      service.get(validUrl);
+
+      expect($windowMock.localStorage.getItem)
+        .toHaveBeenCalledWith(CACHE_PREFIX + validUrl);
+      expect(service.getLive).toHaveBeenCalledWith(validUrl);
+
+      expect($windowMock.localStorage.getItem.calls.count()).toBe(1);
+      expect(service.getLive.calls.count()).toBe(1);
+
+      // change mocked response from cache
+      $windowMock.localStorage.getItem.and.returnValue(responseMockStringified);
+
+      // second get() call
+      service.get(validUrl);
+
+      expect($windowMock.localStorage.getItem.calls.count()).toBe(2);
+      expect(service.getLive.calls.count()).toBe(1);
+    });
+  });
+
+  describe('remove(validUrl)', function() {
+    beforeEach(function() {
+      spyOn($windowMock.localStorage, 'removeItem');
+    });
+
+    it('should remove a single item cached item', function() {
+      service.remove(validUrl);
+      expect($windowMock.localStorage.removeItem)
+        .toHaveBeenCalledWith(CACHE_PREFIX + validUrl);
+    });
+  });
+
+  describe('removeAll()', function() {
+    beforeEach(function() {
+      spyOn($windowMock.localStorage, 'removeItem');
+    });
+
+    it('should remove all elements that have a valid key-name format', function() {
+      service.removeAll();
+
+      expect($windowMock.localStorage.removeItem.calls.count()).toBe(2);
+      expect($windowMock.localStorage.removeItem).toHaveBeenCalledWith('lsc_/');
+      expect($windowMock.localStorage.removeItem).toHaveBeenCalledWith('lsc_http://test');
     });
   });
 });
