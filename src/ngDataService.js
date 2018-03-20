5ad9507b62b4fe8983525ca67c50e5cdcf5cbbd0
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2018-03-20 23:25:31 +0200

    REFACTORED ngDataService as an ES6 class

--- src/ngDataService.js
+++ src/ngDataService.js
@@ -1,93 +1,76 @@
-(function() {
-  'use strict';
+const runningQueries = {};
+const CACHE_PREFIX = 'lsc_';
+const CACHE_REGEX = /^lsc_((http(s)?\:\/\/)|\/)/;
 
-  // @angular
-  ngDataService.$inject = ['$http', '$window', '$q'];
-
-  angular
-    .module('ng-data-service', [])
-    .service('ngDataService', ngDataService);
-
-  // @constants
-  var VALID_URL_REGEIX = /^((http(s)?\:\/\/)|\/)/;
-  var CACHE_PREFIX = 'lsc_';
-  var CACHE_REGEX = /^lsc_((http(s)?\:\/\/)|\/)/;
-
-  // @cache with request promises
-  var runningQueries = {};
-
-  // @private
-  function validateUrl(url) {
-    if(!VALID_URL_REGEIX.test(url)) {
-      var callee = arguments.callee.caller.toString();
-      var errorMessage = 'ngDataService.' + callee + '(url)' +
-        + 'invalid url "' + url + '"';
-
-      throw errorMessage;
-    }
-  }
-
-  function onRequestSuccess(url, value) {
-    delete runningQueries[url];
-    this.storage.setItem(CACHE_PREFIX + url, JSON.stringify(value));
-
-    return value;
-  }
-
-  function onRequestError(url, error, status) {
-    delete runningQueries[url];
-    console.error('HTTP GET [' + url + '] failed with status code ' + status);
-
-    return error;
-  }
-
-  // @constructor
-  function ngDataService($http, $window, $q) {
+class NgDataService {
+  constructor($http, $window) {
     this.$http = $http;
     this.storage = $window.localStorage;
-    this.$q = $q;
   };
 
-  ngDataService.prototype.get = function(url) {
-    validateUrl(url);
+  isValidUrl(url) {
+    return /^((\/)|https?:\/\/)/.test(url);
+  }
+
+  get(url) {
+    if(!this.isValidUrl(url)) {
+      throw new Error(`NgDataService.get(url), invalid url: ${url}`);
+    }
 
     var cached = this.storage.getItem(CACHE_PREFIX + url);
     if(cached) {
-      var deferred = this.$q.defer();
-      var value = {
-        data: JSON.parse(cached)
-      };
-
-      deferred.resolve(value);
-      return deferred.promise;
+      return Promise.resolve({ data: JSON.parse(cached) });
     }
 
     return this.getLive(url);
   }
 
-  ngDataService.prototype.getLive = function(url) {
-    validateUrl(url);
+  getLive(url) {
+    if(!this.isValidUrl(url)) {
+      throw new Error(`NgDataService.getLive(url), invalid url: ${url}`);
+    }
 
     if(!runningQueries.hasOwnProperty(url)) {
-      runningQueries[url] = this.$http
-        .get(url)
-        .success(onRequestSuccess.bind(this, url))
-        .error(onRequestError.bind(this, url));
+      runningQueries[url] = this.$http.get(url)
+        .then(this.onRequestSuccess.bind(this, url))
+        .catch(this.onRequestError.bind(this, url));
     }
 
     return runningQueries[url];
   }
 
-  ngDataService.prototype.remove = function(url) {
-    validateUrl(url);
+  remove(url) {
+    if(!this.isValidUrl(url)) {
+      throw new Error(`NgDataService.remove(url), invalid url: ${url}`);
+    }
+
     this.storage.removeItem(CACHE_PREFIX + url);
   };
 
-  ngDataService.prototype.removeAll = function() {
+  removeAll() {
     for(var item in this.storage) {
       if(CACHE_REGEX.test(item)) {
         this.storage.removeItem(item);
       }
     }
-  };
-})();
+  }
+
+  onRequestSuccess(url, value) {
+    delete runningQueries[url];
+    this.storage.setItem(CACHE_PREFIX + url, JSON.stringify(value.data));
+
+    return value;
+  }
+  
+  onRequestError(url, error, status) {
+    delete runningQueries[url];
+    console.error('HTTP GET [' + url + '] failed with status code ' + status);
+
+    return error;
+  }
+}
+
+NgDataService.$inject = ['$http', '$window'];
+
+angular.module('ng-data-service', [])
+  .service('ngDataService', NgDataService);
